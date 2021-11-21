791a7890ff0cc227cc4b5eb234e68142e43ff2c8
ma963fre <matofre@hotmail.com>
2021-11-21 22:58:19 +0000

    First Commit

---
+++ src/main/java/com/simplilearn/ImageFilter/ImageFilterApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.ImageFilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageFilterApplication.class, args);
+	}
+
+}
